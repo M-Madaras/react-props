af5b6e632cd7af97f65747813c235496742752ed
“Mason <masonmadaras@gmail.com>
2022-08-08 14:27:15 -0400

    props

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,9 @@
 
 
-export default function Main({ name, total, isAwesome, fruit}) {
+export default function Main({ name }) {
     return(
         <main>
             <h1>Hello {name}</h1>
-            <h2>Total: {total}</h2>
-            {isAwesome && <h2>Fruit: {fruit}</h2>}
         </main>
     )
 }
