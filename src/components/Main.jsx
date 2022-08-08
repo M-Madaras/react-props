54ede9fe10005c3982caf3be29a27448b8ebdd61
“Mason <masonmadaras@gmail.com>
2022-08-08 14:15:12 -0400

    destructing params

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 
 
-export default function Main() {
+export default function Main({ name, total, isAwesome, fruit}) {
     return(
         <main>
-            <h1>Main</h1>
+            <h1>Hello {name}</h1>
+            <h2>Total: {total}</h2>
+            {isAwesome && <h2>Fruit: {fruit}</h2>}
         </main>
     )
 }
